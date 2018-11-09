f21b94810ebcb15dc533141f1cb0010ae3d10175
FrancoisAudoy <francois.audoy@gmail.com>
2018-11-09 16:04:04 +0100

    Ajout des paquets utiles dans le package.json et projet factice dans le server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,17 +1,34 @@
-'use strict';
-
-//Sample Node server from nodejs documentation
+//'use strict';
 
 const express = require('express');
+const cors = require('cors');
+const bodyparser = require('body-parser');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+const jsonParser = bodyparser.json();
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
+
+app.use(cors());
+
+let listProjet = [];
+
+app.get('/projets', (req, res) => {
+    res.send(JSON.stringify({name : 'First Projet'}));
+});
+
+app.get('/projet/:projet', (req,res) => {
+    res.send("Les backlogs du projet " + req.params.projet);
+});
+
+app.put('/projets',jsonParser, (req, res) => {
+    res.end(JSON.stringify({name : 'First Projet'}));
+});
+
+app.delete('/projets', jsonParser, (req,res) => {
+    
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
-

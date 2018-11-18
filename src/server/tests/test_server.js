77d07d59d9d8f869105e43550813c99d738e13dd
FrancoisAudoy <francois.audoy@gmail.com>
2018-11-18 16:27:32 +0100

    Configuration de npm test utilisant mocha, et ajout d'un test

---
+++ src/server/tests/test_server.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const assert = require('assert');
+
+describe("Server", () => {
+    describe("#TODO", () => {
+	it("TODO", () => {
+	    assert.equal(1,2);
+	});
+	it('should pass', () =>{
+	    assert(true,"Test à réaliser");
+	});
+    });
+});

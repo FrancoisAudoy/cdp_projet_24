d0e90a683e897b5a56ec22dfd5f301aaae8a509e
Charles Pellegrini <charles.pellegrini@etu.u-bordeaux.fr>
2018-11-20 21:43:05 +0100

    Première version du fichier décrivant les schémas Mongoose

---
+++ src/server/models.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var IssueSchema = new Schema({
+  description: {type: String, required: true, max: 1000},
+  difficulty: {type: String, enum: ['', 'easy', 'medium', 'hard'], default: ''},
+  priority: {type: Number, default: 0}
+});
+
+var BacklogSchema = new Schema({
+  project: {type: Schema.Types.ObjectId, ref: 'Project', required: true},
+  name: {type: String, required: true, max: 100},
+  issues: [IssueSchema]
+});
+module.exports.Backlog = mongoose.model('Backlog', BacklogSchema);
+
+var ProjectSchema = new Schema({
+  name: {type: String, required: true, max: 100},
+});
+module.exports.Project = mongoose.model('Project', ProjectSchema);

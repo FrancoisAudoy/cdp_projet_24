003454e372eea1799f3f65f27d25e4cf0ab3d629
Charles Pellegrini <charles.pellegrini@etu.u-bordeaux.fr>
2018-11-21 05:04:33 +0100

    Avancement de server.js   - Remise en place du mode strict   - Suppression du module
    package.json :
      - suppression de la dependance a body-parse, on utilise maintenant le middleware express.json
      - mise en place d'une reconnection automatique a la db si la premiere connexion echoue
      - desactivation du buffer mongoose pour simplifier les tests pendant ce sprint
      - mise en place des differentes routes
      - premiere implemetation de GET /projects, POST /projects, GET /backlogs, POST /backlogs

--- src/server/server.js
+++ src/server/server.js
@@ -1,40 +1,111 @@
-//'use strict';
+'use strict';
 
 const express = require('express');
 const cors = require('cors');
-const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
+const models = require('./models');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
 var DBURL = 'mongodb://mongodb:27017/cdp';
 
-mongoose.connect(DBURL, { useNewUrlParser: true }).catch(function (error) {
-    console.log('Can\'t connect to MongoDB, error : ', error);
-});
+// MongoDB driver autoreconnects only after the first successfull connect.
+// So we need to implement this ourselves.
+var connectOrRetryLater = function(){
+  mongoose.connect(DBURL, { useNewUrlParser: true }).catch(function (error) {
+    if (error && error.message.match(/failed to connect to server .* on first connect/)) {
+      console.log('Can\'t connect to MongoDB, error : ', error);
+      setTimeout(connectOrRetryLater, 2000);
+    }
+  });
+};
+connectOrRetryLater();
+mongoose.set('bufferCommands', false); // Disabling buffering for now
 
-
-const jsonParser = bodyparser.json();
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-let listProjet = [];
-
-app.get('/projets', (req, res) => {
-    res.send(JSON.stringify({name : 'First Projet'}));
+// obtient la liste des projets
+app.get('/projects', (req, res) => {
+  models.Project.find(function(error, projects){
+    if (error)
+      res.status(400).json(error);
+    else
+      res.json(projects);
+  });
 });
 
-app.get('/projet/:projet', (req,res) => {
-    res.send("Les backlogs du projet " + req.params.projet);
+// crée un projet
+app.post('/projects', (req, res) => {
+  var project = new models.Project();
+  project.name = req.body.name;
+  project.save(function(error, project){
+    if(error)
+      res.status(400).json(error);
+    else
+      res.status(201).json({msg : 'Added project'});
+  });
 });
 
-app.put('/projets',jsonParser, (req, res) => {
-    res.end(JSON.stringify({name : 'First Projet'}));
+// obtient un projet à partir de son identifiant
+app.get('/projects/:project', (req,res) => {
+  // TODO
 });
 
-app.delete('/projets', jsonParser, (req,res) => {
-    
+// modifie un projet
+app.put('/projects:project', (req, res) => {
+  // TODO
+});
+
+// supprime un projet
+app.delete('/projects:project', (req,res) => {
+  // TODO
+});
+
+// obtient la liste des backlogs
+app.get('/backlogs', (req, res) => {
+  models.Backlog.find(function(error, backlogs){
+    if (error)
+      res.status(400).json(error);
+    else
+      res.json(backlogs);
+  });
+});
+
+// crée un backlog appartenant au projet
+app.post('/backlogs', (req, res) => {
+  var backlog = new models.Backlog();
+  // TODO check if there is a project, check if it already has a backlog
+  backlog.projectID = req.body.projectID;
+  backlog.name = req.body.name;
+  backlog.save(function(error, backlog){
+    if(error)
+      res.status(400).json(error);
+    else
+      res.status(201).json({msg : 'Added Backlog'});
+  });
+});
+
+// obtient un backlog à partir de son identifiant
+app.get('/backlogs/:backlog', (req,res) => {
+  // TODO
+});
+
+// modifie un backlog
+app.put('/backlogs/:backlog', (req, res) => {
+  // TODO
+});
+
+// supprime un backlog
+app.delete('/backlogs/:backlog', (req,res) => {
+  // TODO
+});
+
+// ajoute une issue à un backlog
+app.post('/backlogs/:backlog/issue', (req, res) => {
+  // TODO
 });
 
 app.listen(PORT, HOST);

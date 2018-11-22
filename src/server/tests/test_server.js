acea0898a4a1f334be07035a3b1ab8e2c0d4196f
FrancoisAudoy <francois.audoy@gmail.com>
2018-11-22 14:15:43 -0500

    Ajout des tests get et put sur le chemin /projets Suppression des dépendences inutiles pour le docker dans package.json Correction de travis.yml pour installer les packet pour les tests

--- src/server/tests/test_server.js
+++ src/server/tests/test_server.js
@@ -1,14 +1,54 @@
 'use strict'
+const base_url = '/express';
 
 const assert = require('assert');
+const server = require('../server');
+const should = require('should');
+const request = require('supertest');
 
 describe("Server", () => {
-    describe("#TODO", () => {
-	it("TODO", () => {
-	    assert.equal(1,2);
+    describe("Database", () => {
+	it('get empty', (done) =>{
+	    request('localhost:8080')
+		.get('/projects')
+		.expect('Content-Type', /json/)
+		.end((err, res) => {
+		    if (err){
+			console.log(err);
+			return done(err);
+			}
+			res.body.should.be.instanceof(Array);
+			assert(res.body == 0, res.body);
+		    done();
+		});
 	});
-	it('should pass', () =>{
-	    assert(true,"Test à réaliser");
+
+	it('post - get not empty', (done) => {
+	    let data = {name:"projet un"};
+	    console.log(JSON.stringify(data));
+	    request('localhost:8080')
+		.post('/projects')
+		.send(JSON.stringify(data))
+		.expect(201)
+		.end((err, res) => {
+			if(err){
+				console.log('Error code : ' + err);
+				assert(false);
+			}
+		});
+	    
+	     request('localhost:8080')
+		.get('/projects')
+		.expect('Content-Type', /json/)
+		.end((err, res) => {
+		    if (err){
+			return done(err);
+		    }
+		    res.body.should.be.instanceof(Array);
+		    res.body.should.containEql(data);
+		    done();
+		});
+	    
 	});
     });
 });

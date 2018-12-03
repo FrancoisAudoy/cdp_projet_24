fad0455351ad9d749772a3ecc49394f5cf5d93ce
mmayolini <maxime.mayolini@gmail.com>
2018-12-03 16:07:34 +0100

    ajout des fichiers React

---
+++ src/cdp_24_app/src/listProj.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class listProj extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Liste des projets</h2>
+            </div>
+        );
+    }
+}
+
+export default listProj;

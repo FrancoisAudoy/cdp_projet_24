1b28aa7ad73d0601bdba3d5179bf3c02bdad725d
mmayolini <maxime.mayolini@gmail.com>
2018-12-04 19:14:50 +0100

    affichage backlog et tâches

--- src/cdp_24_app/src/listIssues.js
+++ src/cdp_24_app/src/listIssues.js
@@ -1,12 +1,40 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import { getBacklogByProjectId } from "./query";
 
 class listIssues extends Component {
+
     render() {
-        return (
-            <div>
-                <h2>Liste d'issues</h2>
-            </div>
-        );
+        let splitUrl = window.location.pathname.split("/");
+        let listType = splitUrl[1];
+        let projectName = splitUrl[2];
+        let listUS, res;
+        let backlog = getBacklogByProjectId(projectName);
+
+        if (backlog !== null && backlog !== undefined)
+            listUS = backlog.map((US) =>
+                <li>{US.description},{US.priority},{US.difficulty}</li>);
+
+        if(listType === "backlog") {
+            return (
+                <div>
+                    <h2>Backlog du projet "{projectName}"</h2>
+                    <ul>{listUS}</ul>
+                    <Link to={`/backlog/` + this.props.projectName + `/add`}>+</Link>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div>
+                    <h2>Tâches du projet "{projectName}"</h2>
+                    <ul>{listUS}</ul>
+                    <Link to={`/backlog/` + this.props.projectName + `/add`}>+</Link>
+                </div>
+            );
+        }
+        
     }
 }
 
--- src/cdp_24_app/src/listProj.js
+++ src/cdp_24_app/src/listProj.js
@@ -1,36 +1,36 @@
 import React, { Component } from "react";
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import {getAllProjects} from "./query";
+import { getAllProjects } from "./query";
 
 let str = '/backlog/toto';
 
 class listProj extends Component {
 
-    requestAllProject(){
-	return getAllProjects();
+    requestAllProject() {
+        return getAllProjects();
     }
-    
-    render() {
-	let listProject;
-	let allProjects = this.requestAllProject();
 
-	if(allProjects !== null || allProjects !== undefined)
-	listProject = allProjects.map((proj) =>
-				      <li> <Link to={'/backlog/' + proj.name}> {proj.name}</Link> </li>);
+    render() {
+        let listProject;
+        let allProjects = this.requestAllProject();
+
+        if (allProjects !== null && allProjects !== undefined)
+            listProject = allProjects.map((proj) =>
+                <li> <Link to={'/backlog/' + proj.name}> {proj.name}</Link> </li>);
 
         return (
-		<div>
+            <div>
                 <h2>Liste des projets</h2>
-		<div class="container">
-		<div class="row">
-		<div class="col-sm col-9">
-		<ul class="list-group m-5" id="list_proj">{listProject}</ul>
-		<Link class="btn-primary rounded-circle" to={`/backlog`}>+</Link>
-		</div>
-		</div>
-		</div>
-	    </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm col-9">
+                            <ul class="list-group m-5" id="list_proj">{listProject}</ul>
+                            <Link class="btn-primary rounded-circle" to={`/backlog`}>+</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         );
     }

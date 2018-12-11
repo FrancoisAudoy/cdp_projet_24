ff864cb4afdcd0b62d94dbc8c60f16559ddd6569
Charles Pellegrini <charles.pellegrini@etu.u-bordeaux.fr>
2018-12-11 21:06:42 +0100

    Modification de query.js pour que getAllProjects renvoie la Promise utilisée Modification de listProj.js pour utiliser correctement cette Promise de manière asynchrone

--- src/cdp_24_app/src/listProj.js
+++ src/cdp_24_app/src/listProj.js
@@ -4,69 +4,63 @@ import Popup from 'reactjs-popup';
 
 import { getAllProjects, createProject, deleteProjectById } from "./query";
 
-
 class listProj extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: [],
+    };
+    this.sendNewProjet = this.sendNewProjet.bind(this);
+  }
 
-    constructor() {
-        super();
-        this.sendNewProjet = this.sendNewProjet.bind(this);
-    }
+  sendNewProjet() {
+    let field = document.getElementById("nameProj");
+    console.log(field.value);
+    let ret = createProject(field.value);
+    console.log(ret);
+  }
 
-    requestAllProject() {
-        return getAllProjects();
-    }
+  componentDidMount() {
+    getAllProjects()
+      .then(data => this.setState({ projects: data }))
+      .catch(error => {
+        console.log(error);
+        this.setState({ projects: [] });
+      });
+  }
 
-
-    sendNewProjet() {
-        let field = document.getElementById("nameProj");
-        console.log(field.value);
-        let ret = createProject(field.value);
-        console.log(ret);
-    }
-
-    render() {
-        let listProject;
-        let allProjects = this.requestAllProject();
-
-        if (allProjects !== null && allProjects !== undefined)
-            listProject = allProjects.map((proj) =>
-                <li> <Link to={'/backlog/' + proj.name}> {proj.name}</Link>
-                    <Popup trigger={<button className="button btn-primary rounded-circle">Supprimer</button>}
-                        modal>
-
-                        <h1>Êtes-vous sûr de vouloir supprimer le projet "{proj.name}" ?</h1>
-
-                        <button onClick={deleteProjectById(proj.name)}>Supprimer</button>
-
+  render() {
+    const { projects } = this.state;
+    let listProject;
+    console.log(this.state.projects);
+    return (
+      <div>
+        <h2>Liste des projets</h2>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm col-9">
+              <ul className="list-group m-5" id="list_proj">{
+                projects.map(proj =>
+                  <li> <Link to={'/backlog/' + proj.name}> {proj.name}</Link>
+                    <Popup trigger={<button className="button btn-primary rounded-circle">Supprimer</button>} modal>
+                      <h1>Êtes-vous sûr de vouloir supprimer le projet "{proj.name}" ?</h1>
+                      <button onClick={() => deleteProjectById(proj._id)}>Supprimer</button>
                     </Popup>
-                </li>);
-
-        return (
-            <div>
-                <h2>Liste des projets</h2>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-sm col-9">
-                            <ul className="list-group m-5" id="list_proj">{listProject}</ul>
-
-                            <Popup trigger={<button className="button btn-primary rounded-circle">+</button>}
-                                modal>
-
-                                <h1> Nouveau Projet</h1>
-
-                                <form>
-                                    Nom : <input type="text" id="nameProj" />
-                                    <input type="submit" id="accept" value="create" onClick={this.sendNewProjet} />
-                                </form>
-
-                            </Popup>
-                        </div>
-                    </div>
-                </div>
+                  </li>)
+              }</ul>
+              <Popup trigger={<button className="button btn-primary rounded-circle">+</button>} modal>
+                <h1> Nouveau Projet</h1>
+                <form>
+                  Nom : <input type="text" id="nameProj" />
+                  <input type="submit" id="accept" value="create" onClick={this.sendNewProjet} />
+                </form>
+              </Popup>
             </div>
-
-        );
-    }
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default listProj;
--- src/cdp_24_app/src/query.js
+++ src/cdp_24_app/src/query.js
@@ -2,52 +2,79 @@
 // TODO Refactoring
 // TODO rendre les fonctions asynchrones
 
-function useFetchParam(Method, Data){
-    return { credentials: 'include',
-	     method : Method,
-	     mode: 'no-cors',
-	     body: Data,
-	     headers:{
-		 'Accept' : 'application/json',
-		 'Content-Type': 'application/json'}};
+function useFetchParam(httpMethod, data){
+  return { credentials: 'include',
+           method : httpMethod,
+           mode: 'no-cors',
+           body: JSON.stringify(data),
+           headers:{
+            'Accept' : 'application/json',
+            'Content-Type': 'application/json'}};
 }
 
-function error(Code){
-    return !(Code >= 200 && Code < 300);
+function isError(Code){
+  return !(Code >= 200 && Code < 300);
 }
 
 let BASE_URL = '/express';
 
-export function getAllProjects(){
-    fetch(BASE_URL + '/projects?format=json')
-	.then((response) => {
-	    if (!error(response.status)) {
-		var json = response.json();
-		return json;
-	    } else {
-		console.log("getAllProjects : unhandled error code : %d", response.status);
-		//console.log(JSON.parse(response));
-		console.log(json);
-		return {};
-	    }
-	});
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
 }
 
-export async function createProject(projectName){
-    let jsonObject = {name: projectName};
-    let paramPOST = useFetchParam('POST', JSON.stringify(jsonObject));
+export function getAllProjects(){
+  return fetch(BASE_URL + '/projects')
+    .then(handleErrors)
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    });
+}
 
-    await fetch(BASE_URL + '/projects?format=json',
-		paramPOST)
-	.then((response) => {
-	    if (!error(response.status)) {
-		return response.json();
-	    } else {
-		console.log("createProject : unhandled error code : %d", response.status);
-		console.log(response.json());
-		return {};
-	    }
-	});
+
+/*export async function createProject(projectName){
+  let jsonObject = {name: projectName};
+  let paramPOST = useFetchParam('POST', jsonObject);
+
+  await fetch(BASE_URL + '/projects', paramPOST)
+    .then(handleErrors)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+    .catch(error => {
+      console.log(error);
+      //console.log("createProject : unhandled error code : %d", response.status);
+      //console.log(response.json());
+      //return {};
+    });
+
+/* const req = new XMLHttpRequest();
+    req.open('POST', BASE_URL + '/projects', false);
+  req.setRequestHeader('Content-Type','application/json; charset=utf-8');
+  req.send(JSON.stringify(jsonObject));*/
+  
+//}*/
+
+export function createProject(projectName){
+  let jsonObject = {name: projectName};
+  let paramPOST = useFetchParam('POST', jsonObject);
+
+  fetch(BASE_URL + '/projects', paramPOST)
+    .then(handleErrors)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+    .catch(error => {
+      console.log(error);
+      //console.log("createProject : unhandled error code : %d", response.status);
+      //console.log(response.json());
+      //return {};
+    });
 
 /* const req = new XMLHttpRequest();
     req.open('POST', BASE_URL + '/projects', false);
@@ -59,32 +86,31 @@ export async function createProject(projectName){
 // check url for injection
 export function getProjectById(projectId){
     fetch(BASE_URL + '/projects/'+projectId)
-	.then((response) => {
-	    if (!error(response.status)) {
-		var json = JSON.parse(response.responseText);
-		return json;
-	    } else {
-		console.log("getAllProjects : unhandled error code : %d", response.status);
-		console.log(JSON.parse(response.responseText));
-		return {};
-	    }
-	});
+        .then((response) => {
+            if (!isError(response.status)) {
+                var json = JSON.parse(response.responseText);
+                return json;
+            } else {
+                console.log("getAllProjects : unhandled error code : %d", response.status);
+                console.log(JSON.parse(response.responseText));
+                return {};
+            }
+        });
 }
 
 // check url for injection
 export function deleteProjectById(projectId){
-    fetch(BASE_URL + '/projects/'+projectId, useFetchParam('DELETE',null))
+    fetch(BASE_URL + '/projects/'+projectId, useFetchParam('DELETE', null))
 	.then((response) => {
-	    if (!error(response.status)) {
-		var json = JSON.parse(response.responseText);
-		return json;
-	    } else {
-		console.log("getAllProjects : unhandled error code : %d", response.status);
-		console.log(JSON.parse(response.responseText));
-		return {};
-	    }
-	});
-  
+            if (!isError(response.status)) {
+                var json = JSON.parse(response.responseText);
+                return json;
+            } else {
+                console.log("getAllProjects : unhandled error code : %d", response.status);
+                console.log(JSON.parse(response.responseText));
+                return {};
+            }
+        });
 }
 
 export function getAllBacklogs(){
@@ -103,44 +129,43 @@ export function getAllBacklogs(){
 
 export function getBacklogByProjectId(projectId){
     fetch(BASE_URL + '/backlogs/fromProjectId/'+projectId)
-	.then((response) => {
-	    if (!error(response.status)) {
-		console.log(response.responseText);
-		var json = JSON.parse(response.responseText);
-		return json;
-	    } else {
-		console.log("getAllBacklogs : unhandled error code : %d", response.status);
-		console.log(JSON.parse(response.responseText));
-		return {};
-	    }
+        .then((response) => {
+            if (!isError(response.status)) {
+                console.log(response.responseText);
+                var json = JSON.parse(response.responseText);
+                return json;
+            } else {
+                console.log("getAllBacklogs : unhandled error code : %d", response.status);
+                console.log(JSON.parse(response.responseText));
+                return {};
+            }
 	});
-
 }
 
 export function createBacklog(jsonObject){
-    fetch(BASE_URL + '/backlogs', useFetchParam('POST', JSON.stringify(jsonObject)))
-	.then((response) => {
-	    if (!error(response.status)) {
-		console.log("response : %s", response.responseText);
-		return {};
-	    } else {
-		console.log("createBacklog : unhandled error code : %d", response.status);
-		console.log(JSON.parse(response.responseText));
-		return {};
-	    }
+    fetch(BASE_URL + '/backlogs', useFetchParam('POST', jsonObject))
+        .then((response) => {
+            if (!isError(response.status)) {
+                console.log("response : %s", response.responseText);
+                return {};
+            } else {
+                console.log("createBacklog : unhandled error code : %d", response.status);
+                console.log(JSON.parse(response.responseText));
+                return {};
+            }
 	});
 }
 
 //Should check projectId url injection
 export function addIssueToBacklog(backlogId, issue){
-    fetch(BASE_URL + '/backlogs/'+backlogId+'/issue', useFetchParam("POST",JSON.stringify(issue)))
-	.then((response) => {
-	    if (!error(response.status)) {
-		console.log("response : %s", response.responseText);
-	    } else {
-		console.log("addIssueToBacklog : unhandled error code : %d", response.status);
-		console.log(JSON.parse(response.responseText));
-		return {};
-	    }
-	});
+    fetch(BASE_URL + '/backlogs/'+backlogId+'/issue', useFetchParam("POST", issue))
+        .then((response) => {
+            if (!isError(response.status)) {
+                console.log("response : %s", response.responseText);
+            } else {
+                console.log("addIssueToBacklog : unhandled error code : %d", response.status);
+                console.log(JSON.parse(response.responseText));
+                return {};
+            }
+        });
 }
